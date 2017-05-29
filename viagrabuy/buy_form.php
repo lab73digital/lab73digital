aeef9026dcf8c8acd3618132019e32847c2ab074
Andrey <zandyur@gmail.com>
2017-05-29 17:14:47 +0300

    viagrabuy

---
+++ viagrabuy/buy_form.php
@@ -0,0 +1,20 @@
+<?php
+$dosage = $_POST["dosage"];
+$packing = $_POST["packing"];
+$delivery = $_POST["delivery"];
+$pay = $_POST["pay"];
+$tel = $_POST["tel"];
+$price = $_POST["price"];
+
+$myemail = "a.zadorozhniy@lab73.digital";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$message_to_myemail = "<b>Дозировка:</b> $dosage мг<br>
+<b>Фасовка:</b> $packing табл.<br>
+<b>Способ доставки:</b> $delivery<br>
+<b>Оплата:</b> $pay<br>
+<b>Номер телефона:</b> +380 $tel<br>
+<b>Сумма заказа:</b> $price грн.";
+mail($myemail, "Регистрация на igrashop.info", $message_to_myemail, $headers);
