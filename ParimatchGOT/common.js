d8f3647036852eaf1ccd7b9c44a58c74829ea8b7
AndriyZadorozhniy <zandyur@gmail.com>
2017-10-11 12:39:35 +0300

    goty

--- ParimatchGOT/common.js
+++ ParimatchGOT/common.js
@@ -229,6 +229,7 @@ $(function () {
     app._init();
     $('#fblogin').on('click', function () {
         FB.login(function(response) {
+            console.log(response);
             if (response.authResponse) {
                 console.log('Welcome!  Fetching your information.... ');
                 FB.api('/me', function(response) {
