3a72c3f935b10f9e4a9425ccf4d229a313da04ec
Volodymyr Trykoz <volodymyrtz@gmail.com>
2018-04-16 17:40:39 +0300

    min js

--- vipratox/js/scroll-jquery.js
+++
@@ -1,9 +0,0 @@
-$("a[href^='#']").off().on("click", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href');
-    var h = $('header').height();
-
-    var top = $(id).offset().top - h;
-
-    $('body,html').stop(true).animate({scrollTop: top}, 1000);
-});
